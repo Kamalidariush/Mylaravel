b1f5288c44e727f68fa16217f8d9ecc8ad01f648
kamalidariush@gmail.com <kamalidariush@gmail.com>
2026-09-21 09:43:58 +0330

    feat: enable OpenTelemetry runtime metrics3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(
-            \Iamfarhad\Prometheus\Http\Middleware\PrometheusMetricsMiddleware::class
-        );
-
         $middleware->alias([
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
